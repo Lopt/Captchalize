b37d8693e1da1714e1b5f4fdc26d5beee75146f0
leto <leto.larp@googlemail.com>
2013-07-05 22:51:32 +0200

    added erode and dilate as slot function

---
+++ src/cap/slots/MorphErode.java
@@ -0,0 +1,25 @@
+package cap.slots;
+
+import cap.ISlotFunction;
+import cap.ResultPart;
+import cap.SlotFunction;
+import cap.img.CompoundImage;
+import cap.img.Morph;
+
+/**
+ * Authors: Bernd Schmidt, Robert Könitz
+ */
+public class MorphErode extends SlotFunction<CompoundImage> implements ISlotFunction<CompoundImage> {
+
+    @Override
+    public String getClassName() {
+        return "MorphErode";
+    }
+
+    @Override
+    public ResultPart<CompoundImage> execute(CompoundImage image) {
+        //cap.db.jpa.slots.MorphErode data = this.getModel().getFunctionData();
+
+        return new ResultPart<CompoundImage>(Morph.erode(image));
+    }
+}
